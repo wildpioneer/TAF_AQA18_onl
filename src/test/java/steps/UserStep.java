58066a0ef1496840f38e8e9be03bd99adf5bbd2e
aleksandr <atrostyanko@gmail.com>
2023-01-31 17:54:21 +0100

    изменения

--- src/test/java/steps/UserStep.java
+++ src/test/java/steps/UserStep.java
@@ -7,31 +7,13 @@ import org.openqa.selenium.WebDriver;
 import pages.DashboardPage;
 import pages.LoginPage;
 
-public class UserStep extends BaseStep {
+public class UserStep {
     private LoginPage loginPage;
 
-    public UserStep(WebDriver driver) {
-        super(driver);
-
-        loginPage = new LoginPage(driver);
-    }
-
     public void login(String email, String psw) {
-        loginPage.getEmailInput().sendKeys(email);
-        loginPage.getPassword().sendKeys(psw);
+        loginPage.getEmailInput().setValue(email);
+        loginPage.getPassword().val(psw);
         loginPage.getLogInButton().click();
-    }
-
-    @Step
-    public DashboardPage loginSuccessful(String email, String psw) {
-        login(email, psw);
-
-        return new DashboardPage(driver);
-    }
-
-    @Step
-    public DashboardPage loginSuccessful(User user) {
-        return loginSuccessful(user.getEmail(), user.getPassword());
     }
 
     public LoginPage loginIncorrect(String email, String psw) {
---
+++ src/test/java/tests/SelenideTest.java
@@ -0,0 +1,96 @@
+package tests;
+
+import baseEntities.BaseTest;
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import configuration.ReadProperties;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.CollectionCondition.empty;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.isChrome;
+
+public class SelenideTest extends BaseTest {
+
+    @Test
+    public void loginTest() {
+        open("/");
+
+        $(By.id("name")).setValue(ReadProperties.username());
+        $("#password").setValue(ReadProperties.password());
+        $("#password").val(ReadProperties.password());
+
+        SelenideElement loginButton = $("#button_primary");
+        loginButton
+                .should(exist)
+                .shouldBe(enabled)
+                .click();
+        loginButton.pressEnter();
+        loginButton.append("fff");
+        loginButton.attr("dds");
+
+
+/*
+        $(withText("All Projects"))
+                .shouldBe(visible)
+                .shouldHave(text("All Projects"));
+*/
+
+        $(By.xpath("//div[contains(text(), 'All Projects')]"))
+                .shouldBe(visible)
+                .shouldHave(text("All Projects"));
+
+        $(withText("All Projects"))
+                .shouldBe(visible)
+                .shouldHave(text("All Projects"));
+
+        $(byText("All Projects"))
+                .shouldBe(visible)
+                .shouldHave(text("All Projects"));
+
+        $(byTitle("All Projects"))
+                .shouldBe(visible)
+                .shouldHave(text("All Projects"));
+
+
+        $$(withText("All Projects"))
+                .shouldBe(empty)
+                .shouldBe(size(10))
+                .shouldHave(texts("text 1",
+                        "text 2",
+                        "text 3"))
+                .shouldHave(exactTexts("text 1",
+                        "text 2",
+                        "text 3"));
+
+        $$(withText("All Projects"))
+                .filterBy(text("Expected Text"))
+                .excludeWith(hidden)
+                .findBy(visible);
+
+        isChrome();
+    }
+
+    @Test
+    public void loginTest2() {
+        open("/");
+
+        $(By.id("name")).setValue(ReadProperties.username());
+        $("#password").setValue(ReadProperties.password());
+
+        SelenideElement loginButton = $("#button_primary");
+        loginButton
+                .should(exist)
+                .shouldBe(enabled)
+                .click();
+
+        $(By.xpath("//div[contains(text(), 'All Projects')]"))
+                .shouldBe(visible)
+                .shouldHave(text("All Projects"));
+    }
+}
